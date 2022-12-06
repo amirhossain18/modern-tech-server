c9a9063474463ccc5dfe82e8cf63f7fd70bcbb5f
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-12-06 21:50:22 +0600

    loaded newdata

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express= require('express')
 const app = express()
-const port = 5000
-var cors = require('cors')
+const port = process.env.PORT || 5000;
+const cors = require('cors')
 app.use(cors())
-const categorys = require('../modern-tech-server/Catagory.json')
-
+const category = require('../modern-tech-server/Catagory.json')
+// const catagoryDetails = require('../modern-tech-server/CourseDetails.json')
 
 app.get('/', (req, res) =>{
     res.send('wellcome')
 })
 app.get('/courses', (req, res) =>{
-    res.send(categorys)
+    res.send(category)
 })
 
-app.listen(port , ()=> {console.log(`'port' ${port}`)})
+app.get('/courses/:id', (req, res) =>{
+    // res.send(categorys)
+    const id = req.params.id;
+    console.log(id)
+    const selectedCourse = category.find(c => c.id === id);
+    res.send(selectedCourse)
+    console.log(selectedCourse)
+})
+
+app.listen(port , ()=> {
+    
+    console.log('Your port is runnig', port)
+})
